d2faa1acb5866f59339145ae3001ff95fabbcc31
Elayaraja <elayaraja@railsfactory.org>
2011-11-11 18:21:32 +0530

    db/migrate/20111110062054_change_user_id_data_type_to_profile.rb

---
+++ db/migrate/20111110062054_change_user_id_data_type_to_profile.rb
@@ -0,0 +1,9 @@
+class ChangeUserIdDataTypeToProfile < ActiveRecord::Migration
+  def self.up
+    change_column :profiles, :user_id, :integer
+  end
+
+  def self.down
+    change_column :profiles, :user_id, :integer
+  end
+end
