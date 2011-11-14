bab0344dc5e6f9b3d3d7a06c1eced3effb87b978
Elayaraja <elayaraja@railsfactory.org>
2011-11-14 10:47:48 +0530

    delete migration

--- db/migrate/20111110044034_adduser_id_in_articles.rb
+++
@@ -1,9 +0,0 @@
-class AdduserIdInArticles < ActiveRecord::Migration
-  def self.up
-      add_column :articles, :user_id, :string
-  end
-
-  def self.down
-      remove_column :articles, :user_id
-  end
-end
