1623c818af4e0fddd5c180549440febc1c85b758
Elayaraja <elayaraja@railsfactory.org>
2011-11-14 10:47:57 +0530

    delete migration

--- db/migrate/20111110061045_change_user_id_to_article.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserIdToArticle < ActiveRecord::Migration
-  def self.up
-    add_column :articles, :profile_id, :integer
-  end
-
-  def self.down
-    remove_column :artilces, :profile_id
-  end
-end
