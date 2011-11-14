c3d35e119335e233768052b69b609a5a7088f313
Elayaraja <elayaraja@railsfactory.org>
2011-11-14 10:48:08 +0530

    delete migration

--- db/migrate/20111111144413_change_user_id_data_type_to_profile.rb
+++
@@ -1,9 +0,0 @@
-class ChangeUserIdDataTypeToProfile < ActiveRecord::Migration
-  def self.up
-    change_column :profiles, :user_id, :integer
-  end
-
-  def self.down
-    change_column :profiles, :user_id, :string
-  end
-end
