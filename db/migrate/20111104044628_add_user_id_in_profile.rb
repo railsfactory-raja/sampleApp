a9eb5b13a319ba6b02805fdd4b30a0cc15119395
Elayaraja <elayaraja@railsfactory.org>
2011-11-14 10:47:18 +0530

    delete migration

--- db/migrate/20111104044628_add_user_id_in_profile.rb
+++
@@ -1,9 +0,0 @@
-class AddUserIdInProfile < ActiveRecord::Migration
-  def self.up
-    add_column :profiles, :user_id, :string
-  end
-
-  def self.down
-       remove_column :profiles, :user_id, :string
-  end
-end
